897b7cfbe266398deb70150a6b30e7ae010bb8c1
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-03-20 01:31:20 +0900

    エラー検知用

--- src/app/components/SecondHeading.tsx
+++ src/app/components/SecondHeading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SecondHeading = () => {
   return (
     <div className='mt-14 w-3/4 m-auto'>
-      {/* <div className='text-center p-5 text-xl]'>
+      <div className='text-center p-5 text-xl]'>
         <h1 className='text-2xl'>就労支援A型とは</h1>
       </div>
       <div>
@@ -13,15 +13,15 @@ const SecondHeading = () => {
             障害者総合支援法(旧 障害者自立支援法)に定められた就労支援事業の一つです。 一般企業への就職が困難な障がい者や難病の方の就労機会を提供するとともに、生産活動を通じて、その知識と能力の向上に必要な訓練などの障がい福祉サービスを供給することを目的としています。 障がい者や難病の方と雇用契約を結び、原則として最低賃金を保障するしくみの"雇用型"の障がい福祉サービスです。「ホトうさぎ岐阜」では、年齢層の様々な方が活躍しています。
           </p>
         </div>
-        <div className='p-2 flex justify-end'>
+        {/* <div className='p-2 flex justify-end'>
           <Image
             src='/images/読書うさぎさん.png'
             alt='画像'
             width={100}
             height={100}
           />
-        </div>
-      </div> */}
+        </div> */}
+      </div>
     </div>
   )
 }
