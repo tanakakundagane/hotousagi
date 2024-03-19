fea50aabd5680ff9427bd1ca47f678138682ecab
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-03-20 01:19:45 +0900

    シングルクオテーションに修正しました

--- src/app/components/HeaderImage.tsx
+++ src/app/components/HeaderImage.tsx
@@ -5,7 +5,7 @@ const HeaderImage = () => {
   return (
     <div className='relative top-12 w-full h-[500px] lg:h-[600px]'>
       <Image
-        src="/images/easter-ga68fdeac9_1920.jpg"
+        src='/images/easter-ga68fdeac9_1920.jpg'
         alt='メイン画像'
         fill
         style={{ objectFit: 'cover' }}
