6b874a4ed0d6ba5093545702b986c9f27b97ced4
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-05-30 22:17:08 +0900

    微調整２

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,20 +1,11 @@
 import { Resend } from 'resend';
 import { EmailTemplate } from '../../../../components/email-template';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.React_APP_RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
     const { name, email, phone, message } = await req.json();
-
-    // const userMailData = await resend.emails.send({
-    //   from: 'onboarding@resend.dev',
-    //   to: [email],  // ユーザーのメールアドレス
-    //   subject: `${name}様、お問い合わせありがとうございました`,
-    //   text: `こんにちは ${name}様、\n\nお問い合わせいただきありがとうございます。お問い合わせ内容を確認後、担当者より返信させていただきます。\n\nお問い合わせ内容:\n${message}`,
-    //   react: EmailTemplate({ firstName: name, firstMessage: message, firstEmail: email, firstPhone: phone }),
-    // });
-
 
     const data = await resend.emails.send({
       from: 'hotousagi@nicework.co.jp',
