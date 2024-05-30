7681b9cb0b544365a5a473d550d576b9e4c990d5
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-05-30 23:29:26 +0900

    layout.tsxの変更

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,5 +1,7 @@
+
 import { Resend } from 'resend';
 import { EmailTemplate } from '../../../../components/email-template';
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -7,16 +9,33 @@ export async function POST(req: Request) {
   try {
     const { name, email, phone, message } = await req.json();
 
-    const data = await resend.emails.send({
+    // 送信者に送るメール内容
+    const confirmationEmail = {
+      from: 'hotousagi@nicework.co.jp',
+      to: [`${email}`],
+      subject: 'お問い合わせありがとうございます',
+      text: 'お問合せありがとうございます。３日後以内に返信させていただきます。',
+      html: '<p>お問合せありがとうございます。３日後以内に返信させていただきます。</p>',
+    };
+
+    // 管理者に送るメール内容
+    const adminEmail = {
       from: 'hotousagi@nicework.co.jp',
       to: ['niceworkjukai@gmail.com'],
       subject: `${name}様からのお問い合わせ`,
-      text: `Welcome ${name},Message: ${message}`,
-      react: EmailTemplate({ firstName: `${name}` , firstMessage: `${message}`, firstEmail: `${email}`, firstPhone: `${phone}`}),
-    });
+      text: `Welcome ${name}, Message: ${message}`,
+      react: EmailTemplate({ firstName: `${name}`, firstMessage: `${message}`, firstEmail: `${email}`, firstPhone: `${phone}` }),
+    };
 
-    return Response.json(data);
+    // 両方のメールを送信
+    const [confirmationResponse, adminResponse] = await Promise.all([
+      resend.emails.send(confirmationEmail),
+      resend.emails.send(adminEmail)
+    ]);
+
+    return NextResponse.json({ confirmationResponse, adminResponse });
   } catch (error) {
-    return Response.json({ error });
+    console.error('Error sending email:', error);
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
 }
