48c38ab5d70d0f242eb622534048b571eb4da271
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-06-25 23:52:34 +0900

    document.tsxの移動２

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
-        {/* Google Search Console の HTML メタタグをここに追加 */}
         <meta name="google-site-verification" content="dtTPoNAo_BjJd0U4foJJTceYIi4_9_g2Expm0Q6kOHQ" />
       </Head>
       <body>
