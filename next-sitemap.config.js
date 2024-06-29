62f420f8484f36f26ba245c63143850e8cfad30a
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-06-29 23:22:06 +0900

    sitemapの修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,15 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://hotousagi.nicework.co.jp/',
   generateRobotsTxt: true, // (Optional) Generate a robots.txt file
   // ...other options
+  outDir: 'public', // 出力ディレクトリを明示的に指定
+  sitemapSize: 5000, // 1つのサイトマップに含まれるURLの最大数
+  changefreq: 'monthly', // 更新頻度
+  priority: 0.7, // 優先度
+  exclude: ['/api/*'], // 除外するパス（必要に応じて）
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+    await config.transform(config, '/contact'),
+    await config.transform(config, '/information'),
+    await config.transform(config, '/userguide'),
+  ],
 };
