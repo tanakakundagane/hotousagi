e7b52ea7b2334c0bcaa6cdc01211553776259685
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-07-01 22:16:36 +0900

    _document.tsxの作成

---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const _document = () => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
+
+export default _document
