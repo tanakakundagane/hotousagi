174231e866bf83e32ac1c841138733139f1b019f
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-07-01 22:20:49 +0900

    _documentの作成

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Head from 'next/head'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
 
-const _document = () => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default _document
+export default MyApp;
